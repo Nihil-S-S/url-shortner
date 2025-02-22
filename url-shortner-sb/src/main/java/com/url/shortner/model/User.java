70e368426a069f3bbe93d20a76eb8846577d0ac4
Nihil <Nihil.Ss@ril.com>
2025-02-22 17:57:15 +0530

    jwtUtils completed, response to do next

--- url-shortner-sb/src/main/java/com/url/shortner/model/User.java
+++ url-shortner-sb/src/main/java/com/url/shortner/model/User.java
@@ -2,9 +2,10 @@ package com.url.shortner.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Data
 @Table(name = "app_users")
 public class User {
 
@@ -15,4 +16,23 @@ public class User {
     private String userName;
     private String password;
     private String role = "ROLE_USER";
+
+    public String getEmailId() {
+        return emailId;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
